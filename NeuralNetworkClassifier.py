4011ae760d274d7e27354765064745602c9fa5ae
cyrilyared <cyril@yared.info>
2018-08-26 17:49:00 -0400

    Add option to save and load weights

--- NeuralNetworkClassifier.py
+++ NeuralNetworkClassifier.py
@@ -1,12 +1,15 @@
 import numpy as np
 
 class NNMulticlass:
-    def __init__(self, nodes, learningParams, randomSeed):
+    def __init__(self, nodes, learningParams, randomSeed, weights):
         self.inputNode, self.hiddenNode, self.outputNode = nodes
         self.lambd, self.epochs, self.learningRate = learningParams
         if randomSeed:
             np.random.seed(randomSeed)
-        self.weight1, self.weight2 = self._generateWeights()
+        if weights:
+            self.weight1, self.weight2 = weights
+        else:
+            self.weight1, self.weight2 = self._generateWeights()
     
     def _generateWeights(self):
         weight1 = np.random.uniform(-1.0, 1.0, size=(self.hiddenNode, self.inputNode + 1))
@@ -33,7 +36,7 @@ class NNMulticlass:
         regularizationCost = (np.sum(np.square(self.weight1[:,1:])) + np.sum(np.square(self.weight2[:,1:])))*(self.lambd/float(2*m))
         return J + regularizationCost
 
-    def _forwardPropogation(self, X):
+    def _forwardPropagation(self, X):
         activation1 = self._addBiasCol(X)
         z2 = activation1.dot(np.transpose(self.weight1))
         activation2 = self._addBiasCol(self._sigmoid(z2))
@@ -41,7 +44,7 @@ class NNMulticlass:
         activation3 = self._sigmoid(z3)
         return activation1, z2, activation2, z3, activation3
 
-    def _backpropogation(self, activation1, z2, activation2, activation3, y):
+    def _backpropagation(self, activation1, z2, activation2, activation3, y):
         sigma3 = activation3 - y;
         sigma2 = sigma3.dot(self.weight2[:,1:])*self._derivativeSigmoid(z2)
         delta1 = np.transpose(sigma2).dot(activation1)
@@ -49,8 +52,8 @@ class NNMulticlass:
         return delta1, delta2
 
     def _trainingStep(self, X, y):
-        activation1, z2, activation2, z3, activation3 = self._forwardPropogation(X)
-        delta1, delta2 = self._backpropogation(activation1, z2, activation2, activation3, y)
+        activation1, z2, activation2, z3, activation3 = self._forwardPropagation(X)
+        delta1, delta2 = self._backpropagation(activation1, z2, activation2, activation3, y)
         m = X.shape[0]
         delta1 = delta1/float(m) + np.column_stack((np.zeros((delta1.shape[0], 1), dtype=int), self.weight1[:,1:]))*self.lambd/float(m)
         delta2 = delta2/float(m) + np.column_stack((np.zeros((delta2.shape[0], 1), dtype=int), self.weight2[:,1:]))*self.lambd/float(m)
@@ -69,22 +72,31 @@ class NNMulticlass:
 
     def predict(self, X):
         Xtest = X.copy()
-        activation1, z2, activation2, z3, activation3 = self._forwardPropogation(Xtest)
+        activation1, z2, activation2, z3, activation3 = self._forwardPropagation(Xtest)
         return np.argmax(activation3, axis = 1)
 
     def softmaxProbability(self, X):
         Xtest = X.copy()
-        activation1, z2, activation2, z3, activation3 = self._forwardPropogation(Xtest)
+        activation1, z2, activation2, z3, activation3 = self._forwardPropagation(Xtest)
         return np.exp(np.transpose(activation3))/np.sum(np.exp(np.transpose(activation3)))
 
     def accuracy(self, X, y):
         hypothesis = self.predict(X)
         return np.sum(y == hypothesis, axis=0)/float(X.shape[0])
 
+    def saveWeights(self):
+        np.savetxt(raw_input("Enter the filename for the document that will contain weight1: "), self.weight1, delimiter = ',')
+        np.savetxt(raw_input("Enter the filename for the document that will contain weight2: "), self.weight2, delimiter = ',')
 
-def getData(type):
-    filename = raw_input("What is the filename of the file containing the " + type +  " data: ")
-    return np.genfromtxt(filename, delimiter=',').astype(int)
+
+def getData(filetype, datatype):
+    filename = raw_input("What is the filename of the file containing the " + filetype +  " data: ")
+    return np.genfromtxt(filename, delimiter=',').astype(datatype)
+
+def loadWeights():
+    weight1 = getData("weight1", float)
+    weight2 = getData("weight2", float)
+    return weight1, weight2
 
 def checkDimensions(array):
     if array.ndim == 1:
@@ -115,14 +127,15 @@ def normalizeData(min, max, mean, X):
     return X
 
 if __name__ == "__main__":
-    trainingData = getData("training")
+    trainingData = getData("training", float)
     trainingData = checkDimensions(trainingData)
-    Xtrain, ytrain = trainingData[:, :-1], trainingData[:, -1]
+    Xtrain, ytrain = trainingData[:, :-1], trainingData[:, -1].astype(int)
 
     min, max, mean = getMinMaxMean(Xtrain)
     min, max, mean, Xtrain, trimIndex = trimFeatures(min, max, mean, Xtrain)
     Xtrain = normalizeData(min, max, mean, Xtrain)
     
+    # Parameters for the neural network.
     hiddenNode = 250
     outputNode = 10
     lambd = 0.3
@@ -130,14 +143,16 @@ if __name__ == "__main__":
     learningRate = 0.08
     randomSeed = None
     inputNode = Xtrain.shape[1]
+    weights = None  # Replace with loadWeights() to use custom weights from file.
     
-    NN = NNMulticlass((inputNode, hiddenNode, outputNode), (lambd, epochs, learningRate), randomSeed)
+    NN = NNMulticlass((inputNode, hiddenNode, outputNode), (lambd, epochs, learningRate), randomSeed, weights)
 
     NN.train(Xtrain, ytrain)
     print("Training Accuracy: %.2f%%" % (NN.accuracy(Xtrain, ytrain)*100))
     
-    testingData = getData("testing")
+    testingData = getData("testing", float)
     testingData = checkDimensions(testingData)
-    Xtest, ytest = testingData[:, :-1], testingData[:, -1]
+    Xtest, ytest = testingData[:, :-1], testingData[:, -1].astype(int)
     Xtest = normalizeData(min, max, mean, np.delete(Xtest, trimIndex, axis=1))
     print("Testing Accuracy: %.2f%%" % (NN.accuracy(Xtest, ytest)*100))
+
--- README.md
+++ README.md
@@ -8,16 +8,21 @@ By utilizing the parameters in the original file, it is able to get an 82.83% te
 
 This neural network has three layers, an input layer, a hidden layer and an output layer. The number of nodes in each layer is completely customizable with the parameters provided at the bottom of the Python file. This implementation of the network considers bias.
 
-The network first obtains the data and separates the features from the target variable. It then scales the features with mean normalization. Any features that do not contain useful data (where all values are identical for all training examples) are trimmed. The training examples are then passed to the `train` function of an instance of the class `NNMulticlass`.
+The network first obtains the data and separates the features from the target variable. It then scales the features with mean normalization. Any features that do not contain useful data (where all values are identical for all training examples) are trimmed. The training examples are then passed to the `train` method of an instance of the class `NNMulticlass`.
 
 The target variable `y` is passed through a one-hot encoder. The network then uses a vectorized implementation of forward propagation and backpropagation to update the weights. The cost is computed with the logistic cross-entropy error function. Note that the gradients and the cost are regularized. The cost is minimized with batch gradient descent.
 
-After training with the training data, the test data is passed through forward propagation to compute the accuracy. The predict function can be used to predict an outcome `y` given an input `X`. 
+After training with the training data, the test data is passed through forward propagation to compute the accuracy. The `NN.predict()` method can be used to predict an outcome `y` given an input `X`. 
 
 
 ## Training Data Format
 
 The training data should be formatted as follows. Each row is a new training example with comma-delimited data (I would recommend using a CSV file). The first `n - 1` numbers (where `n` is the number of columns) represent the features (X<sup>(i)</sup>). The last number represents the classification for that training example (y<sup>(i)</sup>).
+
+
+## Saving and Loading Weights
+
+Weights can be saved after training the neural network and loaded to use the neural network to make predictions. To save the weights, call `NN.saveWeights()` after training the network. To load the weights into the network, set `weights = loadWeights()` in the parameters section. It will then ask for two separate files containing the weights between the input and hidden layer and the weights between the hidden and output layer. This file should be in a comma-delimited format. I would comment the line `NN.train(Xtrain, ytrain)` if you do not want to further train the weights.
 
 
 ## Future Improvements
@@ -30,6 +35,5 @@ I created this network after completing the Machine Learning course offered thro
 * Employ another more advanced optimization function (conjugate gradient, BFGS, L-BFGS).
 * Increase and parametrize the number of layers in the network.
 * Customize the formats for the training and testing data.
-* Add an option to print the weights into a file and to load weights from a file for forward propagation.
 
 
